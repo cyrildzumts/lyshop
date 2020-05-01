e49a726e1c9b02db4b9f060766becce03c33c775
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-05-01 22:33:57 +0200

    minor update

--- catalog/models.py
+++ catalog/models.py
@@ -310,8 +310,9 @@ class ProductImage(models.Model):
 def generate_product_sku(sender, instance, created, **kwargs):
     if created:
         logger.debug(f'Generating SKU for newly created Product variant {instance.name}')
-        code = f"{instance.product.category.code}{instance.product.brand.code}" + str(instance.id).zfill(conf.PRODUCT_NUMBER_LENGTH)
-        logger.debug(f'SKU for Product variant {instance.name} generated : {code}')
+        sku = f"{instance.product.category.code}{instance.product.brand.code}" + str(instance.id).zfill(conf.PRODUCT_NUMBER_LENGTH)
+        logger.debug(f'SKU for Product variant {instance.name} generated : {sku}')
+        sku_int = int(sku)
         logger.debug("Saving generated SKU in the product ...")
-        updated_rows_count = ProductVariant.objects.filter(pk=instance.pk).update(sku=code, article_number=int(code))
+        updated_rows_count = ProductVariant.objects.filter(pk=instance.pk).update(sku=sku, article_number=sku_int)
         logger.info(f"SKU created for Product Variant {instance.name}. affected Rows : {updated_rows_count}")
