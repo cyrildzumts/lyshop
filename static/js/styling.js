513f18882ee86747ff10e336fcd35fe488b0df7f
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-06-03 23:33:51 +0200

    WIP : minor update

--- static/js/styling.js
+++ static/js/styling.js
@@ -1,23 +1,23 @@
-$('.js-custom-input').on('click', function(event){
+$('.js-custom-input .input-value').on('click', function(event){
     $(this).toggle();
-    $('input', this).toggle();
+    $('.input-edit-wrapper', $(this).parent()).toggle();
 });
 
+/*
 $('.js-custom-input .js-edit').on('click', function(event){
     $(this).parent().toggle();
-    $(this).sibblings('input').toggle();
+    $(this).siblings('input').toggle();
 });
+*/
 
 $('.js-custom-input input').on('keyup change', function(event){
     var $el = $(this);
-    $el.parent().html($el.val());
+    $el.parent().siblings('.input-value').html($el.val());
 });
 
 $('.js-custom-input .js-edit-close').on('click', function(event){
-    var $el = $(this).sibblings('input');
-    $el.parent().html($el.val());
+    var $el = $(this).siblings('input');
+    $el.parent().siblings('.input-value').html($el.val());
     $(this).parent().toggle();
-    $(this).hide();
-    $el.hide();
 });
 
