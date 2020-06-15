fdf562f6506c41b211e6c051fa6ce39239674aea
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-06-16 00:38:04 +0200

    minor update

--- orders/views.py
+++ orders/views.py
@@ -77,6 +77,7 @@ def checkout(request):
             logger.debug(f"Selected Payment Option : {payment_option}")
             if payment_option == commons.PAYMENT_PAY_WITH_PAY:
                 order = orders_service.create_order_from_cart(user=request.user)
+                logger.debug("Order ready. Now preparing payment data")
                 payment_data = {
                     'requester_name': settings.PAY_USERNAME,
                     'amount': cart.amount,
