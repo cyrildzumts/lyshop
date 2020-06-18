bb2e31da0d75692ba13d0b5bf44583882716fb0d
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-06-18 16:47:52 +0200

    minor update

--- lyshop/settings.py
+++ lyshop/settings.py
@@ -251,7 +251,7 @@ LOGGING = {
             'propagate': True,
         },
         'django.request': {
-            'handlers': ['mail_admins'],
+            'handlers': ['mail_admins', 'file'],
             'level': 'ERROR',
             'propagate': False,
         },
