84a1687a8bc910aa8fd825a5af582851c85fcca8
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-06-20 21:53:55 +0200

    minor update

--- static/js/accounts.js
+++ static/js/accounts.js
@@ -26,11 +26,16 @@ $(document).ready(function(){
     init();
     $('.js-edit-form').on('click', function(event){
         var ctx = $($(this).data('target'));
+        $(this).addClass('disabled');
         activate_editable_inputs(ctx);
     });
 
     $('.js-form-edit-cancel').on('click', function(event){
+        event.preventDefault();
         var ctx = $($(this).data('target'));
+        var hide_el = $($(this).data('hide'));
+        hide_el.hide();
+        $('.js-edit-form').removeClass('disabled');
         deactivate_editable_inputs(ctx);
     });
 });
