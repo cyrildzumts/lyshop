583a21d9cba7c6575583093a6f16b0cf7b3c120b
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-06-21 23:31:19 +0200

    minor update

--- static/js/accounts.js
+++ static/js/accounts.js
@@ -7,19 +7,20 @@ function account_form_validation(){
 function activate_editable_inputs(context){
     console.debug("activating editableinputs");
     var $editable_inputs = $('input.js-editable', context);
-    $editable_inputs.addClass('editable');
+    $editable_inputs.addClass('editable').prop('disabled', false);
+
 }
 
 function deactivate_editable_inputs(context){
     console.debug("deactivating editableinputs");
     var $editable_inputs = $('input.js-editable', context);
-    $editable_inputs.removeClass('editable');
+    $editable_inputs.removeClass('editable').prop('disabled', true);;
 }
 
 
 function init(){
     var $editable_inputs = $('input.js-editable');
-    $editable_inputs.removeClass('editable');
+    $editable_inputs.removeClass('editable').prop('disabled', true);;
     $('#form-controls').hide();
 }
 
