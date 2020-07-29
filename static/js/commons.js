3dad0a4900bf8e27c63c38d18fd1e9755cfaf541
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-07-29 23:41:15 +0200

    added brand list filter for catalog

---
+++ static/js/commons.js
@@ -0,0 +1,32 @@
+
+
+var ListFilter = (function(){
+    function ListFilter(){
+        console.log("creating ListFilter instance");
+        this.init();
+        console.log("ListFilter instance created");
+    };
+
+    ListFilter.prototype.init = function(){
+        console.log("ListFilter instance initializing");
+        $('.js-list-filter').on('keyup', function(event){
+            event.stopPropagation();
+            var value = this.value.trim().toLowerCase();
+            var target_container = this.getAttribute('data-target');
+            var el = this.getAttribute('data-element');
+            $('#' + target_container + " " +  el).filter(function(){
+                $(this).toggle(this.getAttribute('data-value').toLowerCase().includes(value));
+            });
+        });
+
+        console.log("ListFilter instance initialized");
+    };
+
+
+    return ListFilter;
+})();
+
+
+$(document).ready(function(){
+    var listfilter = new ListFilter();
+    });
