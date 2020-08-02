9cf45feb29933515aa3374bb14c04bb970e8addb
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-08-02 23:45:51 +0200

    minor update

--- orders/urls.py
+++ orders/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('checkout-success/<uuid:order_uuid>/', views.checkout_success, name='checkout-success'),
     path('checkout-failed/<uuid:order_uuid>/', views.checkout_failed, name='checkout-failed'),
     path('checkout-redirect-payment/<uuid:request_uuid>/', views.checkout_redirect_payment, name='checkout-redirect-payment'),
+    path('orders/', views.orders, name='orders'),
+    path('order-details/<uuid:order_uuid>/', views.order_detail, name='order-detail'),
 ]
