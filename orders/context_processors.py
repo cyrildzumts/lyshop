ab2bdb29681cc46b2e095eb557e85754e55013f0
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-08-02 20:49:36 +0200

    WIP

--- orders/context_processors.py
+++ orders/context_processors.py
@@ -1,9 +1,14 @@
 from orders.models import Order
 
 def order_context(request):
-
+    if request.user.is_authenticated :
+        qs = Order.objects.filter(user=request.user)
+        recent_orders = Order.objects.filter(user=request.user)[:3]
+    else :
+        qs = Order.objects.none()
+        recent_orders = qs
     context = {
-        'recent_orders': Order.objects.filter(user=request.user)[:3],
-        'order_list': Order.objects.filter(user=request.user)
+        'recent_orders': ,recent_orders
+        'order_list': qs
     }
     return context
