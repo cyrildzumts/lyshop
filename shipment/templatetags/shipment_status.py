80936b89ca3d9c1ceba62d783c8cf955cc91156f
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-08-03 20:03:31 +0200

    add log for custom tag filters

--- shipment/templatetags/shipment_status.py
+++ shipment/templatetags/shipment_status.py
@@ -1,6 +1,10 @@
 from django import template
 from lyshop import utils
 from shipment import constants as Constants
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 register = template.Library()
 
@@ -9,12 +13,16 @@ register = template.Library()
 def tuple_key(value):
     k,v = utils.find_element_by_value_in_tuples(value, Constants.SHIPMENT_STATUS)
     if k is None:
+        logger.info(f"CUSTOM TEMPLATE TAG tuple_key : Could not found key  for value \"{value}\"")
         return value
+    logger.info(f"CUSTOM TEMPLATE TAG tuple_key : found key \"{k}\" for value \"{value}\"")
     return k
 
 @register.filter
 def tuple_value(key):
     k,v = utils.find_element_by_key_in_tuples(key, Constants.SHIPMENT_STATUS)
     if v is None:
+        logger.info(f"CUSTOM TEMPLATE TAG tuple_value : Could not found value \"{v}\" for key \"{key}\"")
         return key
+    logger.info(f"CUSTOM TEMPLATE TAG tuple_value : found value \"{v}\" for key \"{key}\"")
     return v
