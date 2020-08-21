1a939fcd4d0563fc5a52f16e308721c2714ed2b9
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-08-21 23:45:07 +0200

    minor update

--- vendors/views.py
+++ vendors/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db.models import F, Q, Sum, Count
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.decorators import login_required
