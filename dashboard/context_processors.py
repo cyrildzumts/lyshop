a7db2b2a5aea7921245095ed26a9301b5d1eca98
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-08-23 15:17:43 +0200

    refactoring

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 
 def dashboard_context(request):
     context = {
-        'banner' : settings.HOME_URL == request.path or settings.DASHBOARD_ROOT_PATH in request.path or settings.ACCOUNT_ROOT_PATH in request.path
+        'banner' : settings.HOME_URL == request.path or settings.DASHBOARD_ROOT_PATH in request.path or settings.ACCOUNT_ROOT_PATH in request.path or settings.VENDOR_ROOT_PATH in request.path
     }
     return context
