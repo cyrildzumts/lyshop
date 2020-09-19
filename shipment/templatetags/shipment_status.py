4dea304f2ae03281cad72eafbdeb353e8505f50d
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-09-19 23:39:21 +0200

    Minor update

--- shipment/templatetags/shipment_status.py
+++ shipment/templatetags/shipment_status.py
@@ -10,17 +10,17 @@ register = template.Library()
 
 
 @register.filter
-def tuple_key(value):
+def shipment_status_key(value):
     k,v = utils.find_element_by_value_in_tuples(value, Constants.SHIPMENT_STATUS)
     if k is None:
-        logger.info(f"CUSTOM TEMPLATE TAG tuple_key : Could not found key  for value \"{value}\"")
+        logger.info(f" shipment_status_key : Could not found key  for value \"{value}\"")
         return value
     return k
 
 @register.filter
-def tuple_value(key):
+def shipment_status_value(key):
     k,v = utils.find_element_by_key_in_tuples(key, Constants.SHIPMENT_STATUS)
     if v is None:
-        logger.info(f"CUSTOM TEMPLATE TAG tuple_value : Could not found value \"{v}\" for key \"{key}\"")
+        logger.info(f"shipment_status_value : Could not found value for key \"{key}\"")
         return key
     return v
