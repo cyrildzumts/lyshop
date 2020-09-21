be6f3d39e182e79aa79643f1d7145e5cd34b1955
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-09-21 22:38:11 +0200

    minor update

--- orders/views.py
+++ orders/views.py
@@ -40,7 +40,7 @@ def orders(request):
     #logger.debug(f"Filter is valid ? : {is_filter_valid}")
     #logger.debug(f"Cleaned Data : {filter_options.cleaned_data}")
     #logger.debug(f"Error Data : {filter_options.errors}")
-    status_list = getdata.get('order_status',[])
+    status_list = getdata.getlist('order_status',[])
     logger.debug(f"Filter Status option (befor mapping to int) : {status_list}")
     status_not_empty = len(status_list) != 0
     if status_not_empty:
