feb0b44f67696426e5f268a2b83026b4d5bd48a4
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-10-25 19:50:14 +0100

    WIP

--- vendors/views.py
+++ vendors/views.py
@@ -19,6 +19,7 @@ from accounts.models import Account
 from cart.models import Coupon
 from cart.forms import CouponForm
 from orders.models import Order, OrderItem
+from orders import commons as Order_Constants
 from core.filters import filters
 from lyshop import utils, settings
 from vendors import vendors_service
