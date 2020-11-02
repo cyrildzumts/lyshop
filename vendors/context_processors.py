6a59b7d2056b044e5667572841f6b7755040256f
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-11-02 20:36:10 +0100

    WIP

--- vendors/context_processors.py
+++ vendors/context_processors.py
@@ -3,12 +3,14 @@ from django.contrib.auth.models import User
 from vendors import vendors_service
 
 
+def can_add_balance(path):
+    return settings.VENDOR_ROOT_PATH in path or settings.USER_PATH in path or settings.ACCOUNT_ROOT_PATH in path
+
 def vendor_context(request):
-    is_seller = request.user.is_authenticated and vendors_service.is_vendor(user=request.user)
-    show_balance = is_seller and settings.VENDOR_ROOT_PATH in request.path or settings.USER_PATH in request.path
+    is_seller = vendors_service.is_vendor(user=request.user)
     balance = None
     home_variables = {}
-    if show_balance:
+    if is_seller and can_add_balance(request.path):
         balance = vendors_service.get_vendor_balance(request.user)
         home_variables = vendors_service.get_vendor_home_variable(request.user)
 
