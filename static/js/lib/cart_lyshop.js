e04830de3fd0c7bd6accd77f3a47fecdbf81acc8
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-11-03 12:23:38 +0100

    WIP : adding requirejs

---
+++ static/js/lib/cart_lyshop.js
@@ -0,0 +1,39 @@
+define(['ajax'], function(ajax) {
+    'use strict';
+    function Cart(){
+        this.user = "cyrildz"
+        this.items = [1,2,4,85,63];
+        this.total = 25000;
+        this.currency = "XAF";
+        console.log("New Cart created");
+    }
+
+    Cart.prototype.add = function(product_uuid){
+        console.log("Adding product %s into cart", product_uuid);
+    }
+    Cart.prototype.remove = function(product_uuid){
+        console.log("Removing product %s from cart", product_uuid);
+    }
+
+    Cart.prototype.putInWishlist = function(product_uuid){
+        console.log("Puting product %s into wishlist", product_uuid);
+    }
+
+    Cart.prototype.clear = function(){
+        console.log("Clearing Cart");
+    }
+
+    Cart.prototype.addCoupon = function(coupon){
+        console.log("Adding coupon %s into cart", coupon);
+    }
+
+    Cart.prototype.removeCoupon = function(coupon){
+        console.log("Remove coupon %s from cart", coupon);
+    }
+
+    Cart.prototype.isValidCoupon = function(coupon){
+        console.log("Verifying coupon ", coupon);
+    }
+
+    return Cart
+});
---
+++ static/js/lyshopApp.js
@@ -0,0 +1,12 @@
+requirejs.confif({
+    baseUrl :'js/lib',
+    paths:{
+        vendor: '../vendor'
+    }
+});
+
+requirejs(['cart_lyshop', 'ajax', 'vendor/jquery.min'], function(Cart, ajax, LS){
+    console.log("LyshopApp loaded ...");
+    var cart = new Cart();
+    console.log("JQuery version : %s", LS().jquery);
+});
