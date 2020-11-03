1b641c4b70d2b6efaa200629a6bd15b41d2da50d
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-11-03 13:11:41 +0100

    WIP

--- static/js/lyshopApp.js
+++ static/js/lyshopApp.js
@@ -5,8 +5,8 @@ requirejs.config({
     }
 });
 
-requirejs(['cart_lyshop', 'ajax', 'vendor/jquery-3.5.0.min'], function(Cart, ajax, $){
+requirejs(['cart_lyshop', 'ajax', 'vendor/jquery-3.5.0.min'], function(Cart, ajax){
     console.log("LyshopApp loaded ...");
     var cart = new Cart();
-    console.log("JQuery version :", $().jquery);
+    console.log("JQuery version :", $);
 });
