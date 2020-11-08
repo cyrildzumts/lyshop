7d5f40e969397327757e6d7e1e20b8e7352339af
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-11-08 13:50:29 +0100

    WIP : add product attr in cart listing

--- cart/views.py
+++ cart/views.py
@@ -36,7 +36,7 @@ def cart(request):
     context = {
         'cart': cart,
         'cartitems_exist' : cartitems_exist,
-        'item_list' : CartItem.objects.filter(cart=cart),
+        'item_list' : CartItem.objects.select_related('product').prefetch_related('product__attributes').filter(cart=cart),
         'page_title' : settings.SITE_NAME + ' ' + 'Cart',
     }
 
