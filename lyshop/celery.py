ca6feb1b7b10f0b1f3697c24aa77987e94c31f2e
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-11-29 18:37:33 +0100

    WIP

--- lyshop/celery.py
+++ lyshop/celery.py
@@ -18,13 +18,11 @@ app.conf.task_default_routing_key = settings.CELERY_DEFAULT_ROUTING_KEY
 app.conf.beat_schedule = {
     'clean_unpaid_orders': {
         'task': 'dashboard.tasks.cancel_unpaid_orders_task',
-        'schedule' : crontab()
-        #'schedule' : crontab(minute=0, hour='*/3')
+        'schedule' : crontab(minute=0, hour='*/3')
     },
     'clean_coupons': {
         'task': 'cart.tasks.clean_coupons_tasks',
-        'schedule' : crontab()
-        #'schedule' : crontab(minute=0, hour=0)
+        'schedule' : crontab(minute=0, hour=0)
     }
 }
 app.autodiscover_tasks()
