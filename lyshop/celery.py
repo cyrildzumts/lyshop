d2c3f37668b4ef1ae58c04ca991594e4b70abb4b
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-11-29 12:20:04 +0100

    WIP

--- lyshop/celery.py
+++ lyshop/celery.py
@@ -18,7 +18,7 @@ app.conf.task_default_exchange_type = settings.CELERY_DEFAULT_EXCHANGE_TYPE
 app.conf.task_default_routing_key = settings.CELERY_DEFAULT_ROUTING_KEY
 app.conf.beat_schedule = {
     'clean_unpaid_orders': {
-        'task': 'lyshop.cancel_unpaid_orders_task',
+        'task': 'task.cancel_unpaid_orders_task',
         'schedule' : crontab(minute=0, hour='*/3')
     }
 }
