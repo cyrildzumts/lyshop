341c2333538608d847ea717eebe3ac117a076ce5
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-12-16 00:55:53 +0100

    minor update

--- lyshop/settings.py
+++ lyshop/settings.py
@@ -149,8 +149,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'inventory.middleware.VisitorCounter',
-    #'inventory.middleware.UniqueIPCounter',
+    'inventory.middleware.VisitorCounter',
+    'inventory.middleware.UniqueIPCounter',
     
 ]
 
