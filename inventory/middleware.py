2b1912a82835acef856971ffe7438758923c87a7
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-12-15 23:45:07 +0100

    minor update

--- inventory/middleware.py
+++ inventory/middleware.py
@@ -11,7 +11,7 @@ class VisitorCounter:
     def __call__(self, request):
         logger.info("VisitorCounter started...")
         v, created = Visitor.objects.get_or_create(url=request.path)
-        Visitor.objects.update(pk=v.pk, hits=F('hits') + 1)
+        Visitor.objects.filter(pk=v.pk).update(hits=F('hits') + 1)
         response = self.get_response(request)
         logger.info("VisitorCounter finished...")
         return response
@@ -24,7 +24,7 @@ class UniqueIPCounter:
     def __call__(self, request):
         logger.info("UniquevisitorCounter started...")
         v, created = UniqueIP.objects.get_or_create(ip_address=request.META.REMOTE_ADDR)
-        UniqueIP.objects.update(pk=v.pk, hits=F('hits') + 1)
+        UniqueIP.objects.filter(pk=v.pk).update(hits=F('hits') + 1)
         response = self.get_response(request)
         logger.info("UniquevisitorCounter finished...")
         return response
