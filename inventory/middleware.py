65754b9f65bbcce670fd51ca605f01e9792bd2ae
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-12-15 23:33:19 +0100

    minor update

--- inventory/middleware.py
+++ inventory/middleware.py
@@ -1,16 +1,18 @@
 from inventory.models import Visitor, UniqueIP
 from django.db.models import F
+import logging
 
+logger = logging.getLogger(__name__)
 
 class VisitorCounter:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-
-        Visitor.objects.updatete_or_create(defaults={'hits' : F('hits')+1},url=request.path)
+        logger.info("VisitorCounter started...")
+        Visitor.objects.update_or_create(defaults={'hits' : F('hits')+1},url=request.path)
         response = self.get_response(request)
-
+        logger.info("VisitorCounter finished...")
         return response
 
 
@@ -19,8 +21,8 @@ class UniqueIPCounter:
         self.get_response = get_response
 
     def __call__(self, request):
-
+        logger.info("UniquevisitorCounter started...")
         UniqueIP.objects.update_or_create(defaults={'hits' : F('hits')+1}, ip_address=request.META.REMOTE_ADDR)
         response = self.get_response(request)
-
+        logger.info("UniquevisitorCounter finished...")
         return response
