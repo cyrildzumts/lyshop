d6e5f49dfca7183efdb4a7d6efdd632a1a35e5f7
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-12-17 09:25:18 +0100

    WIP

--- inventory/middleware.py
+++ inventory/middleware.py
@@ -4,17 +4,19 @@ from django.utils import timezone
 import logging
 
 logger = logging.getLogger(__name__)
+X_FORWARDED_FOR_HEADER = "X-Forwarded-For"
+REMOTE_ADDR = "REMOTE_ADDR"
+IP_SEP = ','
 
 class VisitorCounter:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        logger.info(f"VisitorCounter started... for path {request.path}")
+        logger.info(f"request path : {request.path}")
         v, created = Visitor.objects.get_or_create(url=request.path)
         Visitor.objects.filter(pk=v.pk).update(hits=F('hits') + 1)
         response = self.get_response(request)
-        logger.info(f"VisitorCounter finished...for path {request.path}")
         return response
 
 
@@ -24,11 +26,11 @@ class UniqueIPCounter:
 
     def __call__(self, request):
         
-        if "X-Forwarded-For" in request.META:
-            client_ip = request.META.get('X-Forwarded-For').split(',')[0]
+        if X_FORWARDED_FOR_HEADER in request.META:
+            client_ip = request.META.get(X_FORWARDED_FOR_HEADER).split(IP_SEP)[0]
         else:
-            client_ip = request.META.get('REMOTE_ADDR')
-        logger.info(f"UniquevisitorCounter started...for ip address {client_ip}")
+            client_ip = request.META.get(REMOTE_ADDR)
+        logger.info(f"request client ip : {client_ip}")
         v, created = UniqueIP.objects.get_or_create(ip_address=client_ip)
         UniqueIP.objects.filter(pk=v.pk).update(hits=F('hits') + 1)
         response = self.get_response(request)
