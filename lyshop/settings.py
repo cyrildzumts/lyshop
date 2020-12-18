36e0953714550651b37498b208dbbe41d87e231d
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-12-18 20:26:23 +0100

    WIP

--- lyshop/settings.py
+++ lyshop/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY          =  os.environ['LYSHOP_SECRET_KEY']
+CSRF_COOKIE_SECURE = True
 
 # SITE NAME SETTING
 SITE_NAME           =  os.environ.get('LYSHOP_SITE_NAME', 'LYSHOP')
