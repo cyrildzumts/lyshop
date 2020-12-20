4020723c284992bd5ba660d443f5c3c56fbfdc53
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2020-12-20 13:30:43 +0100

    updated translation strings

--- catalog/strings.py
+++ catalog/strings.py
@@ -3,36 +3,62 @@ from django.utils.translation import gettext_lazy as _
 
 # CATEGORIES NAME USED im CATALOG and DASHBOARD
 
+_("Bags")
+_("Clothes")
 _("Eaux de Toilette")
 _("Eaux de Parfum")
-_("Parfum")
-_("Bags")
-_("Mode")
-_("Clothes")
 _("Electronic")
+_("Mode")
+_("Parfum")
 _("Shoes")
 
 
 # GENDER
-_("WOMEN")
-_("MEN")
 _("BABY BOY")
 _("BABY GIRL")
 _("BOY")
 _("GIRL")
+_("MEN")
 _("NO GENDER")
+_("WOMEN")
 
 # PRODUCT TYPES
+_("ACCESSORIES")
+_("BAG")
 _("SHOES")
 _("SMARTPHONE")
 _("PARFUM")
-_("BAG")
-_("ACCESSORIES")
 
 # ATTIBUTES NAME
-_("Color")
-_("Size")
-_("Capacity")
 _("Battery")
+_("Capacity")
+_("Color")
 _("Material")
 _("RAM")
+_("Size")
+
+_("ONE SIZE")
+_("one size")
+_("One Size")
+
+# COLOR NAME
+_("Beige")
+_("Black")
+_("Blue")
+_("Bordeaux")
+_("Brown")
+_("Caramel")
+_("Coral")
+_("Dark Blue")
+_("Fuchsia")
+_("Gray")
+_("Green")
+_("Olive")
+_("Orange")
+_("Pink")
+_("Purple")
+_("Red")
+_("Sky Blue")
+_("White")
+_("Yellow")
+
