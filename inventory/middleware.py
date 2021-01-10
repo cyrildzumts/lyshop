a66f065858362d1ce168cdd4c120582826670d53
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-01-10 04:52:35 +0100

    minor update

--- inventory/middleware.py
+++ inventory/middleware.py
@@ -8,14 +8,16 @@ X_FORWARDED_FOR_HEADER = "X-Forwarded-For"
 FACEBOOK_REQUEST_QUERY = "fbclid"
 REMOTE_ADDR = "REMOTE_ADDR"
 IP_SEP = ','
+SERVER_NAMES = ['85.214.155.78', '10.221.168.93']
 
 class VisitorCounter:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        v, created = Visitor.objects.get_or_create(url=request.path)
-        Visitor.objects.filter(pk=v.pk).update(hits=F('hits') + 1)
+        if request.META['SERVER_NAME'] not in SERVER_NAMES:
+            v, created = Visitor.objects.get_or_create(url=request.path)
+            Visitor.objects.filter(pk=v.pk).update(hits=F('hits') + 1)
         response = self.get_response(request)
         return response
 
@@ -24,14 +26,16 @@ class FacebookHitCounter:
         self.get_response = get_response
 
     def __call__(self, request):
-        if X_FORWARDED_FOR_HEADER in request.META:
-            client_ip = request.META.get(X_FORWARDED_FOR_HEADER).split(IP_SEP)[0]
-        else:
-            client_ip = request.META.get(REMOTE_ADDR)
-        if request.method == 'GET' and FACEBOOK_REQUEST_QUERY in request.GET.copy():
-            fbclid = request.GET.copy().get(FACEBOOK_REQUEST_QUERY)
-            fblh, created = FacebookLinkHit.objects.get_or_create(fbclid=fbclid, ip_address=client_ip)
-            FacebookLinkHit.objects.filter(pk=fblh.pk).update(hits=F('hits') + 1)
+        if request.META['SERVER_NAME'] not in SERVER_NAMES:
+
+            if X_FORWARDED_FOR_HEADER in request.META:
+                client_ip = request.META.get(X_FORWARDED_FOR_HEADER).split(IP_SEP)[0]
+            else:
+                client_ip = request.META.get(REMOTE_ADDR)
+            if request.method == 'GET' and FACEBOOK_REQUEST_QUERY in request.GET.copy():
+                fbclid = request.GET.copy().get(FACEBOOK_REQUEST_QUERY)
+                fblh, created = FacebookLinkHit.objects.get_or_create(fbclid=fbclid, ip_address=client_ip)
+                FacebookLinkHit.objects.filter(pk=fblh.pk).update(hits=F('hits') + 1)
         response = self.get_response(request)
         return response
 
@@ -41,14 +45,14 @@ class UniqueIPCounter:
         self.get_response = get_response
 
     def __call__(self, request):
-        
-        if X_FORWARDED_FOR_HEADER in request.META:
-            client_ip = request.META.get(X_FORWARDED_FOR_HEADER).split(IP_SEP)[0]
-        else:
-            client_ip = request.META.get(REMOTE_ADDR)
-        logger.info(f"request client ip : {client_ip}")
-        logger.info(f"request path : {request.path}")
-        v, created = UniqueIP.objects.get_or_create(ip_address=client_ip)
-        UniqueIP.objects.filter(pk=v.pk).update(hits=F('hits') + 1)
+        if request.META['SERVER_NAME'] not in SERVER_NAMES:
+            if X_FORWARDED_FOR_HEADER in request.META:
+                client_ip = request.META.get(X_FORWARDED_FOR_HEADER).split(IP_SEP)[0]
+            else:
+                client_ip = request.META.get(REMOTE_ADDR)
+            logger.info(f"request client ip : {client_ip}")
+            logger.info(f"request path : {request.path}")
+            v, created = UniqueIP.objects.get_or_create(ip_address=client_ip)
+            UniqueIP.objects.filter(pk=v.pk).update(hits=F('hits') + 1)
         response = self.get_response(request)
         return response
