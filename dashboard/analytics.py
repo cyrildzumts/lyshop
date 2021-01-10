621ea1e791470b4c8d1089b1ba880359ac96c0f0
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-01-10 04:18:17 +0100

    minor update

--- dashboard/analytics.py
+++ dashboard/analytics.py
@@ -255,6 +255,9 @@ def report_visitors(year=timezone.now().year):
             count = model.objects.filter(created_at__year=year, created_at__month=m).aggregate(count=Sum('hits')).get('count', 0)
             model_data.append(count or 0)
         data.append(model_data)
+    
+    logger.info("Visitor data reports : ")
+    logger.info(data)
 
     report = {
         'labels': ['Visitors', 'Unique Visitors', 'Facebook Visitors'],
