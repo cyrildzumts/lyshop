e50565c9ceedafb7cfc7ff20666f5b2d8799ff04
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-01-10 12:42:35 +0100

    minor update

--- inventory/middleware.py
+++ inventory/middleware.py
@@ -9,7 +9,7 @@ FACEBOOK_REQUEST_QUERY = "fbclid"
 REMOTE_ADDR = "REMOTE_ADDR"
 IP_SEP = ','
 SERVER_NAMES = ['85.214.155.78', '10.221.168.93']
-EXCLUDES_PATHS = ['/api', '/dashboard', ]
+EXCLUDES_PATHS = ['/api', '/dashboard']
 
 class VisitorCounter:
     def __init__(self, get_response):
