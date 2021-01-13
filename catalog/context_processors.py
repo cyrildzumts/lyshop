f13698151b0bf42656372296a256822b42bd8c68
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-01-13 21:25:08 +0100

    minor update

--- catalog/context_processors.py
+++ catalog/context_processors.py
@@ -1,10 +1,12 @@
-from catalog.models import Category, Brand, Product
+from catalog.models import Category, Brand, Product, News
+from django.utils import timezone
 
 def catalog_context(request):
 
     context = {
         'root_category_list': Category.objects.filter(parent=None),
         'brand_list': Brand.objects.all(),
-        'FILTER_CONFIG' : Product.CATALOGUE_FILTER_CONFIG
+        'FILTER_CONFIG' : Product.CATALOGUE_FILTER_CONFIG,
+        'news' : News.objects.filter(is_active=True, end_at__gt=timezone.now()).first()
     }
     return context
