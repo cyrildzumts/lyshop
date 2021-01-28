9f3485579b49dffcd539c6efb6f8b51a53f68d67
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-01-28 11:23:30 +0100

    minor update

--- core/signals.py
+++ core/signals.py
@@ -2,6 +2,7 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from core.tasks import send_mail_task
 from lyshop import settings
 import logging
 
