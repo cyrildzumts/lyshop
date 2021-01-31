700fa882491702d43d823ef735a053007f12cfe4
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-01-31 21:39:12 +0100

    minor update

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,9 +9,9 @@ from catalog import views
 app_name = 'catalog'
 urlpatterns = [
     path('', views.catalog_home, name='catalog-home'),
-    path('sale/', views.catalog_home, name='sale-catalog-home'),
+    path('<str:sale>/', views.catalog_home, name='sale-catalog-home'),
     path('brands/detail/<uuid:brand_uuid>/', views.brand_detail, name='brand-detail'),
-    path('categories/sale/detail/<uuid:category_uuid>/', views.category_detail, name='sale-category-detail'),
+    path('categories/<str:sale>/detail/<uuid:category_uuid>/', views.category_detail, name='sale-category-detail'),
     path('categories/detail/<uuid:category_uuid>/', views.category_detail, name='category-detail'),
     path('categories/products/detail/<uuid:product_uuid>/', views.product_detail, name='product-detail'),
     path('categories/products/variant/detail/<uuid:variant_uuid>/', views.product_variant_detail, name='product-variant-detail'),
