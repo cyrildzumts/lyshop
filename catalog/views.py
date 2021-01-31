46ae4810352ba5a2ad0161c1997074a3c26bda7c
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-01-31 21:14:19 +0100

    minor update

--- catalog/views.py
+++ catalog/views.py
@@ -102,7 +102,7 @@ def category_detail(request, sale=None, category_uuid=None):
 
     queryDict = request.GET.copy()
     field_filter = filters.Filter(Product, queryDict)
-    queryset = field_filter.apply_filter().filter(is_active=True, , sale=sale=='sale')
+    queryset = field_filter.apply_filter().filter(is_active=True, sale=sale=='sale')
     selected_filters = field_filter.selected_filters
     queryset = queryset.filter(filterquery | subcatquery)
     page = request.GET.get('page', 1)
