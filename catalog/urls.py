3e5e81436e055358145ead9e6c56d02395064a7d
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-02-01 20:47:34 +0100

    minor update

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,15 +7,20 @@ from catalog import views
 
 
 app_name = 'catalog'
-urlpatterns = [
+catalog_patterns = [
     path('', views.catalog_home, name='catalog-home'),
-    path('<str:sale>/', views.catalog_home, name='sale-catalog-home'),
-    path('brands/detail/<uuid:brand_uuid>/', views.brand_detail, name='brand-detail'),
-    path('categories/<str:sale>/detail/<uuid:category_uuid>/', views.category_detail, name='sale-category-detail'),
+    path('categories/detail/<uuid:category_uuid>/', views.category_detail, name='sale-category-detail'),
     path('categories/detail/<uuid:category_uuid>/', views.category_detail, name='category-detail'),
     path('categories/products/detail/<uuid:product_uuid>/', views.product_detail, name='product-detail'),
     path('categories/products/variant/detail/<uuid:variant_uuid>/', views.product_variant_detail, name='product-variant-detail'),
     path('products/', views.ProductListView.as_view(), name='products'),
     path('products/product-image/detail/<uuid:image_uuid>/', views.product_image_detail, name='product-image-detail')
+]
+
+urlpatterns = [
+    path('', include(catalog_patterns)),
+    path('<str:sale>/', include(catalog_patterns), name='sale'),
+    path('brands/detail/<uuid:brand_uuid>/', views.brand_detail, name='brand-detail'),
+    
     #path('products/<uuid:product_uuid>', views.ProductDetailView.as_view(), name='product-detail'),
 ]
