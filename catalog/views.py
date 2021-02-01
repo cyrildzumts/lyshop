7e35cc4c2426b3d51370ce68bd4fd406751eeca9
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-02-01 21:22:14 +0100

    minor update

--- catalog/views.py
+++ catalog/views.py
@@ -71,7 +71,6 @@ def catalog_home(request, sale=None,):
     queryset = field_filter.apply_filter().filter(is_active=True)
     selected_filters = field_filter.selected_filters
     sale_category = Product.objects.filter(is_active=True, sale=True).exists()
-    logger.debug(f"catalog_home : sale {sale}")
     if sale == 'sale':
         queryset = queryset.filter(sale=True)
     context = {
