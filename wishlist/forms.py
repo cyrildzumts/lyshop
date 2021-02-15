1fb8103fd0bcc4a40755c977cab1543505a499c8
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-02-15 18:17:47 +0100

    minor update

--- wishlist/forms.py
+++ wishlist/forms.py
@@ -2,7 +2,9 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from wishlist.models import Wishlist, WishlistItem
+import logging
 
+logger = logging.getLogger(__name__)
 
 
 class WishlistForm(forms.ModelForm):
@@ -16,6 +18,7 @@ class WishlistForm(forms.ModelForm):
         super().clean()
         name = self.cleaned_data.get('name')
         customer = self.cleaned_data.get('customer')
+        logger.debug(f"Wishlist clean : {customer} - typeof {type(customer)}")
         if Wishlist.objects.filter(name=name, customer=customer).exists():
             raise ValidationError("Name already in use.")
 
