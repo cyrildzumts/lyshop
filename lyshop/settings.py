70ab6e5ecc2e84d6715af94a86cf4e5c09731e30
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-02-28 22:06:45 +0100

    django rest static update

--- lyshop/settings.py
+++ lyshop/settings.py
@@ -337,8 +337,7 @@ DECIMAL_SEPAROTOR='.'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = [
-    #BASE_DIR + "static",
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
