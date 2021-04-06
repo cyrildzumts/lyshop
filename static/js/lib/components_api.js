e9d38ce4ca35b54f08279891fe5f205977f17812
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-04-06 15:18:57 +0200

    minor update

--- static/js/lib/components_api.js
+++ static/js/lib/components_api.js
@@ -70,6 +70,8 @@ var Tabs = (function(){
         $('.' + tab_preffix + index + " .icon").toggleClass(checked_icon_class, toggle);
         $('.' + tab_preffix + index + " ." + tab_title_class).toggleClass(tab_title_checked, toggle);
         $('#' + tab_content_prefix + index + " .js-tab-next").toggleClass('disabled', !toggle);
+        $('.js-send').toggleClass('disabled', !toggle);
+
     };
     Tabs.prototype.onTabClicked = function(event){
         var tab = parseInt($(event.target).data("index"));
