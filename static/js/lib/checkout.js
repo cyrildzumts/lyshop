d5c6df5d5cb99b4826423e3e7a6296dac59d3a26
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-04-06 13:10:33 +0200

    minor update

--- static/js/lib/checkout.js
+++ static/js/lib/checkout.js
@@ -205,8 +205,8 @@ define([
     };
     Checkout.prototype.update_payment_method = function(){
         console.log("Checkout  update_payment_method :  method %s : type : %s ", this.payment_option, typeof this.payment_option);
-        var methods = PAYMENT_OPTION_METHODS_MAPPING.get(this.payment_option);
-        var is_valid = methods && methods.includes(this.payment_method);
+        var methods = PAYMENT_OPTION_METHODS_MAPPING.get(parseInt(this.payment_option));
+        var is_valid = methods && methods.includes(parseInt(this.payment_method));
         var li_list = $(PAYMENT_METHOD_CONTAINER + " li");
         li_list.hide();
         if(methods){
