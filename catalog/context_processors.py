4500db3836d64709bc679e92e52407f352ecf896
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-05-09 19:59:53 +0200

    started redesign of navigation

--- catalog/context_processors.py
+++ catalog/context_processors.py
@@ -1,4 +1,5 @@
-from catalog.models import Category, Brand, Product, News
+from catalog.models import Category, Brand, Product, News, ProductType
+from catalog import constants as CATALOG_CONSTANT
 from django.utils import timezone
 
 def catalog_context(request):
@@ -7,6 +8,8 @@ def catalog_context(request):
         'root_category_list': Category.objects.filter(parent=None, is_active=True),
         'brand_list': Brand.objects.all(),
         'FILTER_CONFIG' : Product.CATALOGUE_FILTER_CONFIG,
-        'news' : News.objects.filter(is_active=True, end_at__gt=timezone.now()).first()
+        'news' : News.objects.filter(is_active=True, end_at__gt=timezone.now()).first(),
+        'GENDER': CATALOG_CONSTANT.GENDER,
+        'PRODUCT_TYPES': ProductType.objects.all()
     }
     return context
