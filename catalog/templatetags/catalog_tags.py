2017fad38502063d6976c06baf7d0993bcda08b0
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-06-29 20:43:57 +0200

    WIP

--- catalog/templatetags/catalog_tags.py
+++ catalog/templatetags/catalog_tags.py
@@ -67,6 +67,6 @@ def category_page_title(key):
 
 @register.inclusion_tag("tags/navigation_tree.html")
 def nav_tree(category):
-    categories_map = category.children
+    categories_map = category.get('children')
     logger.info("nav_tree tags")
     return {"categories_map": categories_map}
